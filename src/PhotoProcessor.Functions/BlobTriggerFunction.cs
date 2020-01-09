60ff130fbaf5fee5262f67acdf3d6ecba1cdb042
Layla-P <layla.porter@gmail.com>
2020-01-09 12:40:34 +0000

    BlobStorage Trigger now working :-)

--- src/PhotoProcessor.Functions/BlobTriggerFunction.cs
+++ src/PhotoProcessor.Functions/BlobTriggerFunction.cs
@@ -7,7 +7,8 @@ namespace PhotoProcessor.Functions
     public static class BlobTrigger
     {
         [FunctionName("BlobTrigger")]
-        public static void Run([BlobTrigger("images/OriginalImage-{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
+        [StorageAccount("AzureWebJobsStorage")]
+        public static void Run([BlobTrigger("images/UploadsImage-{name}")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
