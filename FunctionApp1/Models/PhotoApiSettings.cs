b0ab4c283aec7be849c86221b79271121f0eb63e
Layla-P <layla.porter@gmail.com>
2020-01-17 13:40:19 +0000

    Removed test projects

--- FunctionApp1/Models/PhotoApiSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace FunctionApp1.Models
-{
-    public class PhotoApiSettings
-    {
-        public string PrivateKey { get; set; }
-        public string AppId { get; set; }
-    }
-}
--- FunctionApp1/Processors/IPhotoProcessor.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace FunctionApp1.Processors
-{
-    public interface IPhotoProcessor
-    {
-        Task<string> Process(string incomingImageUrl, string id, string host);
-    }
-}
